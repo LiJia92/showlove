46ff20c7dd326ab924e844c2226501ecbcbfb31f
Jaceli <243244898@qq.com>
2016-01-19 19:55:12 +0800

    选择图片压缩后显示

--- app/src/main/java/com/android/lovesixgod/showlove/ui/EditActivity.java
+++ app/src/main/java/com/android/lovesixgod/showlove/ui/EditActivity.java
@@ -1,30 +1,25 @@
 package com.android.lovesixgod.showlove.ui;
 
 import android.app.Activity;
-import android.content.ContentResolver;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.text.Editable;
-import android.text.Spannable;
-import android.text.SpannableString;
-import android.text.style.ImageSpan;
 import android.view.View;
-import android.widget.EditText;
+import android.widget.ImageView;
 
 import com.android.lovesixgod.showlove.R;
-
-import java.io.FileNotFoundException;
 
 /**
  * Created by Lijia on 2016-01-19.
  */
 public class EditActivity extends Activity implements View.OnClickListener {
 
-    private EditText editZone;
+//    private EditText editZone;
+
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,7 +27,8 @@ public class EditActivity extends Activity implements View.OnClickListener {
         setContentView(R.layout.activity_edit);
 
         FloatingActionButton addPic = (FloatingActionButton) findViewById(R.id.add_pic);
-        editZone = (EditText) findViewById(R.id.edit_zone);
+//        editZone = (EditText) findViewById(R.id.edit_zone);
+        imageView = (ImageView) findViewById(R.id.image);
         addPic.setOnClickListener(this);
     }
 
@@ -48,29 +44,24 @@ public class EditActivity extends Activity implements View.OnClickListener {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (resultCode == RESULT_OK && requestCode == 0) {
-            ContentResolver resolver = getContentResolver();
-            // 获得图片的uri
             Uri originalUri = data.getData();
-            try {
-                Bitmap originalBitmap = BitmapFactory.decodeStream(resolver.openInputStream(originalUri));
-                ImageSpan imageSpan = new ImageSpan(EditActivity.this, originalBitmap);
-                // 创建一个SpannableString对象，以便插入用ImageSpan对象封装的图像
-                String tempUrl = "<img src=\"" + originalUri.getPath().toString() + "\" />";
-                SpannableString spannableString = new SpannableString(tempUrl);
-                // 用ImageSpan对象替换你指定的字符串
-                spannableString.setSpan(imageSpan, 0, tempUrl.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
-                // 将选择的图片追加到EditText中光标所在位置
-                int index = editZone.getSelectionStart(); // 获取光标所在位置
-                Editable editText = editZone.getEditableText();
-                if (index < 0 || index >= editText.length()) {
-                    editText.append(spannableString);
-                } else {
-                    editText.insert(index, spannableString);
-                }
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
+            BitmapFactory.Options options = new BitmapFactory.Options();
+            options.inJustDecodeBounds = true;
+            BitmapFactory.decodeFile(originalUri.getPath(), options);
+            int height = options.outHeight;
+            int width = options.outWidth;
+            int inSampleSize = 1;
+            int reqHeight = 800;
+            int reqWidth = 480;
+            if (height > reqHeight || width > reqWidth) {
+                final int heightRatio = Math.round((float) height / (float) reqHeight);
+                final int widthRatio = Math.round((float) width / (float) reqWidth);
+                inSampleSize = heightRatio < widthRatio ? heightRatio : widthRatio;
             }
-
+            options.inJustDecodeBounds = false;
+            options.inSampleSize = inSampleSize;
+            Bitmap bitmap = BitmapFactory.decodeFile(originalUri.getPath(), options);
+            imageView.setImageBitmap(bitmap);
         }
     }
 }
